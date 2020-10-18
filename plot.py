a380e2af3cace68039200d70167472ba39934080
Victor Porton <porton@narod.ru>
2020-10-18 19:02:22 +0300

    plotted

--- plot.py
+++ plot.py
@@ -17,21 +17,37 @@ y = f(x)
 x2 = np.arange(x_point-0.1, x_point+0.1, 0.01)
 y2 = f(x2)
 
-figsize = (10, 8)
+figsize = (10, 5)
 cols = 3
-rows = 4
+rows = 2
+
+graph_color = 'blue'
 
 axs = plt.figure(figsize=figsize, constrained_layout=True).subplots(rows, cols)
-axs[0][0].plot(x, y, color='blue')
+axs[0][0].plot(x, y, color=graph_color)
+axs[0][0].set_title("Take an arbitrary function")
 
 axs[0][1].plot(x, y, alpha=0)
-axs[0][1].plot(x2, y2, color='blue')
+axs[0][1].plot(x2, y2, color=graph_color)
+axs[0][1].set_title("Its infinitely small fragment")
 
 axs[0][2].plot(x, y, alpha=0)
-axs[0][2].fill_between(x2, y2-width/2, y2+width/2, color='blue')
+axs[0][2].fill_between(x2, y2-width/2, y2+width/2, color=graph_color)
+axs[0][2].set_title("Topological transformation\nto have infinitely small width")
 
 axs[1][0].plot(x, y, alpha=0)
-axs[1][0].fill_between(x2, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color='blue')
+axs[1][0].fill_between(x2, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
+axs[1][0].set_title("...does not differ of")
 
-# axs.show()
+axs[1][1].plot(x, y, alpha=0)
+for i in range(6):
+    axs[1][1].fill_between(x2 + (i-4)*0.3, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
+axs[1][1].set_title("Shift to infinitely many x positions")
+
+axs[1][2].plot(x, y, alpha=0)
+for i in range(6):
+    axs[1][2].fill_between(x2 + (i-4)*0.3, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
+axs[1][2].plot(x, [f(x_point) for t in x], color='red')
+axs[1][2].set_title("Its y limit point")
+
 plt.show()

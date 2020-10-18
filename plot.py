27efe54db17876219e7e7d39be06d41b4fdac27a
Victor Porton <porton@narod.ru>
2020-10-18 18:34:37 +0300

    rectangle

--- plot.py
+++ plot.py
@@ -8,11 +8,13 @@ def f(x):
 
 min_x = -1.0
 max_x = 1.0
+x_point = 0.5
+width = 0.2
 
 x = np.arange(min_x, max_x, 0.01)
 y = f(x)
 
-x2 = np.arange(-0.1, 0.1, 0.01)
+x2 = np.arange(x_point-0.1, x_point+0.1, 0.01)
 y2 = f(x2)
 
 figsize = (10, 8)
@@ -21,11 +23,15 @@ rows = 4
 
 axs = plt.figure(figsize=figsize, constrained_layout=True).subplots(rows, cols)
 axs[0][0].plot(x, y, color='blue')
+
 axs[0][1].plot(x, y, alpha=0)
 axs[0][1].plot(x2, y2, color='blue')
-axs[0][2].plot(x, y, alpha=0)
-axs[0][2].fill_between(x2, y2-0.1, y2+0.1, color='blue')
 
+axs[0][2].plot(x, y, alpha=0)
+axs[0][2].fill_between(x2, y2-width/2, y2+width/2, color='blue')
+
+axs[1][0].plot(x, y, alpha=0)
+axs[1][0].fill_between(x2, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color='blue')
 
 # axs.show()
 plt.show()

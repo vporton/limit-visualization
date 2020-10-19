e2f6ac0c504c898a1cdc435d22ab0bd6af2c2934
Victor Porton <porton@narod.ru>
2020-10-19 20:13:24 +0300

    misc

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
+import sys
 
 fig = plt.figure()
 
@@ -29,50 +30,58 @@ def make_frame(label):
     ax.plot(x, y, alpha=0)
     return ax
 
-fileCounter = 0
+def fileName(number):
+    return "frame{}.png".format(number)
 
-def fileName():
-    global fileCounter
-    result = "frame{}.png".format(fileCounter)
-    fileCounter += 1
-    return result
 
-ims = []
+def generate_frame0():
+    ax = make_frame("Take an arbitrary function")
+    ax.plot(x, y, color=graph_color)
+    plt.savefig(fileName(0))
 
-ax = make_frame("Take an arbitrary function")
-ax.plot(x, y, color=graph_color)
-plt.savefig(fileName())
+def generate_frame1():
+    ax = make_frame("Its infinitely small fragment")
+    ax.plot(x, y, alpha=0)
+    ax.plot(x2, y2, color=graph_color)
+    plt.savefig(fileName(1))
 
-ax = make_frame("Its infinitely small fragment")
-ax.plot(x, y, alpha=0)
-ax.plot(x2, y2, color=graph_color)
-plt.savefig(fileName())
+def generate_frame2():
+    ax = make_frame("Topological transformation\nto have infinitely small width")
+    ax.plot(x, y, alpha=0)
+    ax.fill_between(x2, y2-width/2, y2+width/2, color=graph_color)
+    plt.savefig(fileName(2))
 
-ax = make_frame("Topological transformation\nto have infinitely small width")
-ax.plot(x, y, alpha=0)
-ax.fill_between(x2, y2-width/2, y2+width/2, color=graph_color)
-plt.savefig(fileName())
+def generate_frame3():
+    ax = make_frame("...does not differ of")
+    ax.plot(x, y, alpha=0)
+    ax.fill_between(x2, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
+    plt.savefig(fileName(3))
 
-ax = make_frame("...does not differ of")
-ax.plot(x, y, alpha=0)
-ax.fill_between(x2, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
-plt.savefig(fileName())
+def generate_frame4():
+    ax = make_frame("Shift to infinitely many x positions.\nThis set is the generalized limit")
+    ax.plot(x, y, alpha=0)
+    for i in range(6):
+        ax.fill_between(x2 + (i-4)*0.3, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
+    plt.savefig(fileName(4))
 
-ax = make_frame("Shift to infinitely many x positions.\nThis set is the generalized limit")
-ax.plot(x, y, alpha=0)
-for i in range(6):
-    ax.fill_between(x2 + (i-4)*0.3, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
-plt.savefig(fileName())
+def generate_frame5():
+    ax = make_frame("Its y limit point is the customary limit")
+    ax.plot(x, y, alpha=0)
+    for i in range(6):
+        ax.fill_between(x2 + (i-4)*0.3, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
+    ax.plot(x, [f(x_point) for t in x], color='red')
+    plt.savefig(fileName(5))
 
-ax = make_frame("Its y limit point is the customary limit")
-ax.plot(x, y, alpha=0)
-for i in range(6):
-    ax.fill_between(x2 + (i-4)*0.3, [f(x_point)-width/2 for t in x2], [f(x_point)+width/2 for t in x2], color=graph_color)
-ax.plot(x, [f(x_point) for t in x], color='red')
-plt.savefig(fileName())
+def generate_frame(number):
+    h = [generate_frame0, generate_frame1, generate_frame2, generate_frame3, generate_frame4, generate_frame5]
+    h[number]()
 
-if os.system("convert -delay 200 -loop 0 frame*.png plot.gif"):
-    raise "Cannot execute `convert`."
+# if os.system("convert -delay 200 -loop 0 frame*.png plot.gif"):
+#     raise "Cannot execute `convert`."
 
-if os.system("rm -f frame*.png"):
-    raise "Cannot remove temp files."
+# if os.system("rm -f frame*.png"):
+#     raise "Cannot remove temp files."
+
+ids = (int(x) for i in sys.argv[1:])
+for id in ids:
+    generate_frame(id)
--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from yapapi.log import enable_default_logger, log_summary, log_event_repr # noqa
 from yapapi.runner import Engine, Task, vm
@@ -14,21 +15,25 @@ async def main(subnet_tag = "testnet"):
     )
 
     async def worker(ctx: WorkContext, tasks):
-        async for task in tasks:
-            ctx.run("/bin/bash", "-c", "cd /root && ./plot.py > log.txt 2>&1")
-            output_file = f"plot.gif"
-            ctx.download_file(f"/root/plot.gif", output_file)
-            ctx.download_file(f"/root/log.txt", f"log.txt")
+        if len(tasks) < 2:
+            raise "Need at least 2 tasks!"
+        async for i, task in enumerate(tasks):
+            frames = ['0', '1', '2'] if i == 0 else ['3', '4', '5']
+            framesStr = ' '.join(frames)
+            ctx.run("/bin/bash", "-c", f"cd /root && ./plot.py {framesStr} > log.txt 2>&1")
+            for frame in frames:
+                ctx.download_file(f"/root/frame{frame}.png", f"frame{frame}.png")
+            # ctx.download_file(f"/root/log.txt", f"log.txt")
             yield ctx.commit()
-            task.accept_task(result=output_file)
+            task.accept_task()
 
-        ctx.log(f"image created.")
+        ctx.log(f"frames created.")
 
     jobs: range = range(0, 1, 1)
     init_overhead: timedelta = timedelta(minutes = 1)
     async with Engine(
         package = package,
-        max_workers = 1,
+        max_workers = 2,
         budget = 1000,
         timeout = init_overhead + timedelta(minutes = 1),
         subnet_tag = subnet_tag,
@@ -37,6 +42,9 @@ async def main(subnet_tag = "testnet"):
 
         async for job in engine.map(worker, [Task(data = job) for job in jobs]):
             print(f"[job done: {job}, result: {job.output}")
+    
+    if os.system("convert -delay 200 -loop 0 frame*.png plot.gif"):
+        raise "Cannot execute `convert`."
 
 
 enable_default_logger()
